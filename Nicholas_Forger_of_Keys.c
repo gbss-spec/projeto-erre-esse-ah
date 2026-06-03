6963469be1a7a572f48a4bff7527fe84a6c8a01c
Givaldo Batista da Silva Sobrinho <gbss@ic.ufal.br>
2026-06-03 12:17:54 -0300

    So Nicholas was born

---
+++ Nicholas_Forger_of_Keys.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*Nicholas tem como objetivo:
+- Ler um par de números primos a1 e a2;
+- Ler um expoente e que seja relativamente primo a (p-1)(q-1);
+- Produzir um arquivo txt com os dois números que formam a chave pública;
+*/
+int main(){
+
+    unsigned long long int //Ao usar unsigned, a variável se torna capaz de representar números positivos maiores em detrimento da capacidade de representar números negativos. 
+
+
+    printf("Bem vindo, filho... Há algo que queiras proteger? Conta-me sobre números interessantes e dar-te-ei trancas em que podes confiar...\n");
+    printf("Conta-me de dois números primos... Que sejam grandes... Com, pelo menos, 6 dígitos...\n")
+    scanf("%")
+
+
+}
