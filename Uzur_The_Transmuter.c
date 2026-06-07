40e4895c2eb1df026b10bac33289a9d53e1e588b
Givaldo Batista da Silva Sobrinho <gbss@ic.ufal.br>
2026-06-07 16:48:17 -0300

    Uzur lê e converte o texto em uma array de int

--- Uzur_The_Transmuter.c
+++ Uzur_The_Transmuter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
 
@@ -8,11 +9,40 @@ A tarefa de Uzur é usar a chave fornecida por Nicholas para criptografar a mens
 
 
 int main(){
-    char message[200];
+    
+    char texto[1001];
+    
+    //printf("Olá, viajante! Imagino que tenhas para mim uma chave e um segredo, não?! Haha-");
+    
+    fgets(texto, sizeof(texto), stdin);
+    
+    int tamanho = strlen(texto);
+    unsigned long long int equiv_num[tamanho]; //Equivalente numérico
+    
+    for (int i = 0; i < tamanho; i++) {
+        equiv_num[i] = (unsigned long long int)texto[i]; 
+    }
+    
+    // Exibindo o resultado
+    printf("\nArray de inteiros gerado:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%llu", equiv_num[i]);
+    }
+    printf("\n");
 
-    //Ler mensagem
-    scanf("%[^\n]", message);
-    printf("%s", message);
+    /*Caminho reverso (para teste)*/
+     for (int i = 0; i < tamanho; i++) {
+         texto[i] = (char)equiv_num[i]; 
+    }
+    printf("\nreverse:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%c", texto[i]);
+    }
+    
+
+
+
+    
     return 0;
 
 }
