e99a8e7de85e06ad430e2d6b060535ba42542ff7
Givaldo Batista da Silva Sobrinho <gbss@ic.ufal.br>
2026-06-07 21:19:47 -0300

    DEsgraça.

--- Uzur_The_Transmuter.c
+++ Uzur_The_Transmuter.c
@@ -7,13 +7,39 @@ A tarefa de Uzur é usar a chave fornecida por Nicholas para criptografar a mens
 
 */
 
+//__________________Exponenciação Modular Rápida (Usada para cifrar os caracteres)
+unsigned long long int ExMoRap(int modulo, int expoente, int equiv_num_car) {
+    int result = 1; //Caso base
+
+    // Loop until exponent becomes 0
+    while(expoente >= 1) {
+        
+        // if odd, multiply result by current x and take modulo
+        if(expoente % 2) {
+            result = (result * modulo) % equiv_num_car;
+            
+            // Reduce exponent by 1 to make it even
+            expoente--;  
+        }
+        
+        // if even, square the base and halve the exponent
+        else {
+            modulo = (modulo * modulo) % equiv_num_car;
+            expoente /= 2;
+        }
+    }
+    return result;
+}
 
 int main(){
     
+    unsigned long long int modulo, expoente;
     char texto[1001];
     
-    //printf("Olá, viajante! Imagino que tenhas para mim uma chave e um segredo, não?! Haha-");
-    
+    printf("Olá, viajante! Diga-me o par de números que compõem sua chave, separando-os com um espaço.");
+    scanf("%llu %llu", &modulo, &expoente);
+
+
     fgets(texto, sizeof(texto), stdin);
     
     int tamanho = strlen(texto);
@@ -22,15 +48,37 @@ int main(){
     for (int i = 0; i < tamanho; i++) {
         equiv_num[i] = (unsigned long long int)texto[i]; 
     }
-    
-    // Exibindo o resultado
+
+
+    //Escrever mensagem criptografada em txt usando exponenciação modular rápida
+    char* filename = "Criptografado.txt";
+
+        FILE* fptr = fopen(filename, "w");
+
+    if(fptr == NULL)
+    {
+        printf("Error: could not open file '%s'.\n", filename);
+        return 1;
+    }
+
+    /* Engrave the Elusive Message on the document */
+    for (int i = 0; i < tamanho; i++) {
+        fprintf("%d ", ExMoRap(modulo, expoente, equiv_num[i]));
+    }
+
+    /* Close the file */
+    fclose(fptr);
+
+
+
+    /*Exibindo o resultado
     printf("\nArray de inteiros gerado:\n");
     for (int i = 0; i < tamanho; i++) {
         printf("%llu", equiv_num[i]);
     }
-    printf("\n");
+    printf("\n");*/
 
-    /*Caminho reverso (para teste)*/
+    /*Caminho reverso (para teste)
      for (int i = 0; i < tamanho; i++) {
          texto[i] = (char)equiv_num[i]; 
     }
@@ -38,6 +86,7 @@ int main(){
     for (int i = 0; i < tamanho; i++) {
         printf("%c", texto[i]);
     }
+    */
     
 
 
